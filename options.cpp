036946e4df852f77f5b7aad69e513337b4cfbce7
Andrey Kutejko <andy128k@gmail.com>
2018-03-20 09:00:19 +0100

    options dialog

--- options.cpp
+++ options.cpp
@@ -2,79 +2,12 @@
 #include "messages.h"
 #include "sound.h"
 
-
-class OptionsChangedCommand: public Command
-{
-    private:
-        bool &fullscreen;
-        float &volume;
-        Area *area;
-        Config *config;
-    
-    public:
-        OptionsChangedCommand(Area *a, Config *config, bool &fs, float &v): 
-            fullscreen(fs), volume(v) {
-            area = a;
-            this->config = config;
-        };
-
-        virtual void doAction() {
-            Screen *screen = area->screen;
-
-            bool oldFullscreen = ein_config_get_fullscreen(config) != 0;
-            float oldVolume = ((float)ein_config_get_volume(config)) / 100.0f;
-
-            if (fullscreen != oldFullscreen) {
-                ein_config_set_fullscreen(config, fullscreen);
-                screen->setMode(VideoMode(800, 600, 24, fullscreen));
-            }
-
-            if (volume != oldVolume) {
-                ein_config_set_volume(config, (int)(volume * 100.0f));
-                sound->setVolume(volume);
-            }
-            area->finishEventLoop();
-        };
-};
-
-
-#define LABEL(y, s) \
-    area.add(new Label(screen, &font, 300, y, 300, 20, Label::ALIGN_LEFT, \
-                Label::ALIGN_MIDDLE, 255,255,255, msg(s)));
-#define CHECKBOX(y, var) \
-    area.add(new Checkbox(screen, 265, y, 20, 20, &font, 255,255,255, L"blue.bmp", \
-                var));
-#define OPTION(y, s, var) LABEL(y, s) CHECKBOX(y, var)
+extern "C" int ein_show_options_window(SDL_Surface*, Config*);
 
 void showOptionsWindow(Area *parentArea, Config *config)
 {
-    Font titleFont(L"nova.ttf", 26);
-    Font font(L"laudcn2.ttf", 14);
-
-    bool fullscreen = ein_config_get_fullscreen(config) != 0;
-    float volume = ((float)ein_config_get_volume(config)) / 100.0f;
-    
-    Screen *screen = parentArea->screen;
-
-    Area area(screen);
-
-    area.add(parentArea);
-    area.add(new Window(screen, 250, 170, 300, 260, L"blue.bmp"));
-    area.add(new Label(screen, &titleFont, 250, 175, 300, 40, Label::ALIGN_CENTER,
-                Label::ALIGN_MIDDLE, 255,255,0, msg(L"options")));
-    OPTION(260, L"fullscreen", fullscreen);
-    
-    area.add(new Label(screen, &font, 265, 330, 300, 20, Label::ALIGN_LEFT,
-                Label::ALIGN_MIDDLE, 255,255,255, msg(L"volume")));
-    area.add(new Slider(screen, 360, 332, 160, 16, volume));
-    
-    ExitCommand exitCmd(area);
-    OptionsChangedCommand okCmd(&area, config, fullscreen, volume);
-    area.add(new Button(screen, 315, 390, 85, 25, &font, 255,255,0, L"blue.bmp", 
-                msg(L"ok"), &okCmd));
-    area.add(new Button(screen, 405, 390, 85, 25, &font, 255,255,0, L"blue.bmp", 
-                msg(L"cancel"), &exitCmd));
-    area.add(new KeyAccel(screen, SDLK_ESCAPE, &exitCmd));
-    area.add(new KeyAccel(screen, SDLK_RETURN, &okCmd));
-    area.run();
+    int quit = ein_show_options_window(parentArea->screen->screen, config);
+    if (quit) {
+        exit(0);
+    }
 }
---
+++ src/ui/component/options_dialog.rs
@@ -0,0 +1,110 @@
+use std::rc::Rc;
+use std::cell::{RefCell, Cell};
+use sdl;
+use sdl::video::{Surface};
+use sdl::event::{Key};
+use sdl2::pixels::Color;
+use sdl2::rect::{Rect};
+use error::*;
+use ui::widget::widget::*;
+use ui::widget::label::*;
+use ui::widget::button::*;
+use ui::widget::checkbox::*;
+use ui::widget::slider::*;
+use ui::widget::window::*;
+use ui::widget::container::*;
+use ui::utils::{HorizontalAlign, VerticalAlign};
+use ui::fonts::*;
+use ui::main_loop::main_loop;
+use ui::background::BLUE_PATTERN;
+use locale::get_language;
+use storage::Storage;
+
+struct Options {
+    fullscreen: Rc<Cell<bool>>,
+    volume: Rc<Cell<f32>>,
+    ok: Cell<bool>,
+}
+
+fn new_options_dialog(storage: &Storage) -> Result<Container<Rc<Options>>> {
+    let rect = Rect::new(250, 170, 300, 260);
+
+    let state = Rc::new(Options {
+        fullscreen: Rc::new(Cell::new(storage.fullscreen)),
+        volume: Rc::new(Cell::new(storage.volume as f32 / 100f32)),
+        ok: Cell::new(false),
+    });
+
+    let mut container = Container::new(rect, state.clone());
+
+    container.add(Box::new(Window::new(rect, BLUE_PATTERN)?));
+    container.add(Box::new(Label {
+        font: title_font()?,
+        text: "Options".to_string(), // msg(L"options")
+        rect: Rect::new(250, 175, 300, 40),
+        color: Color::RGB(255, 255, 0),
+        horizontal_align: HorizontalAlign::Center,
+        vertical_align: VerticalAlign::Middle,
+        shadow: true
+    }));
+
+    container.add(Box::new(Checkbox::new(Rect::new(265, 260, 20, 20), BLUE_PATTERN, state.fullscreen.clone())?));
+
+    container.add(Box::new(Label {
+        font: button_font()?,
+        text: "fullscreen".to_string(), // msg(L"fullscreen")
+        rect: Rect::new(300, 260, 300, 20),
+        color: Color::RGB(255, 255, 255),
+        horizontal_align: HorizontalAlign::Left,
+        vertical_align: VerticalAlign::Middle,
+        shadow: true
+    }));
+
+    container.add(Box::new(Label {
+        font: button_font()?,
+        text: "volume".to_string(), // msg(L"volume")
+        rect: Rect::new(265, 330, 300, 20),
+        color: Color::RGB(255, 255, 255),
+        horizontal_align: HorizontalAlign::Left,
+        vertical_align: VerticalAlign::Middle,
+        shadow: true
+    }));
+
+    container.add(Box::new(Slider::new(Rect::new(360, 332, 160, 16), BLUE_PATTERN, state.volume.clone())?));
+
+    container.add(Box::new({
+        let state_weak = Rc::downgrade(&state);
+        Button::new(Rect::new(315, 390, 85, 25), Color::RGB(255, 255, 0), BLUE_PATTERN, "ok", // i18n
+            Some(Key::Return),
+            move || {
+                if let Some(state) = state_weak.upgrade() {
+                    state.ok.set(true);
+                }
+                Some(Effect::Terminate)
+            }
+        )?
+    }));
+
+    container.add(Box::new(Button::new(Rect::new(405, 390, 85, 25), Color::RGB(255, 255, 0), BLUE_PATTERN, "cancel", // i18n
+        Some(Key::Escape),
+        || Some(Effect::Terminate)
+    )?));
+
+    Ok(container)
+}
+
+#[no_mangle]
+pub fn ein_show_options_window(surface_ptr: * mut sdl::video::ll::SDL_Surface, storage_ptr: *mut ::libc::c_void) -> ::libc::c_int {
+    let surface = sdl::video::Surface { raw: surface_ptr, owned: false };
+    let storage: &mut Storage = unsafe { &mut * (storage_ptr as *mut Storage) };
+    let container = new_options_dialog(storage).unwrap();
+    let quit = main_loop(&surface, &container).unwrap();
+    if container.private.ok.get() {
+        storage.fullscreen = container.private.fullscreen.get();
+        storage.volume = (container.private.volume.get() * 100f32) as u32;
+
+        // screen->setMode(VideoMode(800, 600, 24, container.private.fullscreen.get()));
+        // sound->setVolume(container.private.volume.get());
+    }
+    quit as i32
+}
