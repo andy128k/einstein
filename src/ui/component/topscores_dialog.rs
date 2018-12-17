36027c2415c5f5eb78d33b6f31d418adaf826e5c
Andrey Kutejko <andy128k@gmail.com>
2018-12-17 09:30:45 +0100

    grid for scores

--- src/ui/component/topscores_dialog.rs
+++ src/ui/component/topscores_dialog.rs
@@ -6,6 +6,7 @@ use crate::ui::widget::common::{Background, Border};
 use crate::ui::widget::label::*;
 use crate::ui::widget::dialog_button::*;
 use crate::ui::widget::container::Container;
+use crate::ui::layout::grid::GridBuilder;
 use crate::ui::component::dialog::dialod_widget;
 use crate::resources::messages::Messages;
 use crate::storage::{Scores};
@@ -14,36 +15,35 @@ use crate::util::time::sec_to_str;
 pub fn create_topscores_dialog(scores: &Scores, messages: &Messages, highlight: Option<usize>) -> Container<()> {
     let bg = Background::BLUE_PATTERN;
 
-    let mut container = Container::<()>::container(Size::new(320, 350), bg, Border::Raised);
+    let container = Container::<()>::container(Size::new(320, 350), bg, Border::Raised)
+        .add(10, 10,
+            Label::title(Size::new(300, 40), messages.top_scores).no_action()
+        )
+        .add(10, 50, {
+            let mut grid = GridBuilder::new(Container::container(Size::new(300, 250), None, None), 3, 1);
+            for (i, score) in scores.0.iter().enumerate() {
+                let color = if highlight == Some(i) {
+                    Color::RGB(255, 255, 0)
+                } else {
+                    Color::RGB(255, 255, 255)
+                };
 
-    container.push(10, 10, WidgetMapAction::no_action(
-        Label::title(Size::new(300, 40), messages.top_scores)
-    ));
-
-    let mut pos = 50;
-    for (i, score) in scores.0.iter().enumerate() {
-        let color = if highlight == Some(i) {
-            Color::RGB(255, 255, 0)
-        } else {
-            Color::RGB(255, 255, 255)
-        };
-
-        container.push(10, pos, WidgetMapAction::no_action(
-            Label::new(Size::new(30, 25), &format!("{}.", i + 1), color, HorizontalAlign::Right)
-        ));
-        container.push(50, pos, WidgetMapAction::no_action(
-            Label::new(Size::new(160, 25), &score.name, color, HorizontalAlign::Left)
-        ));
-        container.push(220, pos, WidgetMapAction::no_action(
-            Label::new(Size::new(80, 25), &sec_to_str(score.score), color, HorizontalAlign::Right)
-        ));
-
-        pos += 25;
-    }
-
-    container.push(115, 310,
-        DialogButton::new(Size::new(90, 25), bg, messages.ok, Some(Keycode::Escape), ())
-    );
+                grid = grid
+                    .add(0, i,
+                        Label::new(Size::new(30, 25), &format!("{}.", i + 1), color, HorizontalAlign::Right).no_action()
+                    )
+                    .add(1, i,
+                        Label::new(Size::new(160, 25), &score.name, color, HorizontalAlign::Left).no_action()
+                    )
+                    .add(2, i,
+                        Label::new(Size::new(80, 25), &sec_to_str(score.score), color, HorizontalAlign::Right).no_action()
+                    );
+            }
+            grid.build()
+        })
+        .add(115, 310,
+            DialogButton::new(Size::new(90, 25), bg, messages.ok, Some(Keycode::Escape), ())
+        );
 
     dialod_widget(None, container)
 }
